55b6864c0d7a2da3735b2233fadc033ced46d6e0
Christoffer Riis <Christoffer@Riis.cc>
2022-09-17 02:47:53 +0200

    music works! but not playlist...:(

--- commands/profile.js
+++ commands/profile.js
@@ -1,32 +1,27 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
 
-function profileEmbed(interaction)  {
-    return{
-        "type": "rich",
-        "color": 0xE37800,
-        "title": `${interaction.user.username}'s Profile`,
-        "description": `User info:`,
-        "thumbnail": {url: `${interaction.user.displayAvatarURL({ dynamic: true })}`},
-        "fields": [ { name: 'Username', value: `${interaction.user.username}`, inline: true },
-                    { name: 'Created at', value: `${interaction.user.createdAt}`, inline: true },
-                    { name: 'Joined at', value: `${interaction.member.joinedAt}`, inline: true },
-                    //{ name: 'Status', value: `${interaction.user.presence.status}`, inline: true },
-                    //{ name: 'Activity', value: `${interaction.user.presence.activities[0]}`, inline: true },
-                    { name: 'Roles', value: `${interaction.member.roles.cache.map(role => role.toString()).join(' ')}`}],
-        "timestamp": new Date(),
-        "footer": { text: 'Developed by MrTacoCat#6868', iconURL: 'https://cdn.discordapp.com/avatars/182532419335749632/fca1851401444b5a29aacab533bb64fc.webp' }
-    
-
-    }               
-}
-
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('profile')
         .setDescription('Replies with your profile!'),
-    async execute(interaction) {
-        await interaction.reply({embeds: [profileEmbed(interaction)]})
+        
+    run: async ({interaction}) => {
+        let embed = new EmbedBuilder()
+
+        embed
+        .setColor(0xE37800)
+        .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
+        .setTitle(`${interaction.user.username}'s Profile`)
+        .setDescription(`User info:`)
+        .addFields({ name: 'Username', value: `${interaction.user.username}`, inline: true },
+                    { name: 'Joined at', value: `${interaction.member.joinedAt}`, inline: true },
+                    { name: 'Roles', value: `${interaction.member.roles.cache.map(role => role.toString()).join(' ')}`})
+        .setTimestamp(new Date())
+        
+        await interaction.editReply({
+            embeds: [embed]
+        })
     },
 };
--- main.js
+++ main.js
@@ -15,7 +15,6 @@ const client = new Client({ intents: [	GatewayIntentBits.Guilds,
 	GatewayIntentBits.GuildVoiceStates, 
 	GatewayIntentBits.GuildMembers,
 	GatewayIntentBits.GuildMessages] });
-
 client.slashcommands = new Discord.Collection()
 client.player = new Player(client, {
     ytdlOptions: {
@@ -66,56 +65,3 @@ else {
     })
     client.login(token)
 }
-
-
-
-/*const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
-const { Player } = require('discord-player');
-
-
-const client = new Client({ intents: [	GatewayIntentBits.Guilds, 
-										GatewayIntentBits.GuildVoiceStates, 
-										GatewayIntentBits.GuildMembers,
-										GatewayIntentBits.GuildMessages] });
-
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	client.commands.set(command.data.name, command);
-}
-
-client.Player = new Player(client, {
-	ytdlOptions: {
-		quality: 'highestaudio',
-		highWaterMark: 1 << 25,
-	},
-});
-
-client.on('ready', () => {
-    
-	console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
-
-client.login(token);*/
