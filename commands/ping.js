2b5a8f89d8487474e81583a99367611258191076
Christoffer Riis <Christoffer@Riis.cc>
2023-03-24 22:53:45 +0100

    fixed slash commands

--- commands/ping.js
+++ commands/ping.js
@@ -1,21 +1,11 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Replies with Pong!'),
-    run: async ({interaction}) => {
-        let embed = new EmbedBuilder()
-        
-        embed
-        .setColor(0xE37800)
-        .setTitle(`pong!`)
-        .setTimestamp(new Date())
-        
-    await interaction.editReply({
-        embed: [embed]
-    });
-
-    },
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply('Pong!');
+		await interaction.editReply({ content: 'This my edited second reply!' });
+	},
 };
--- commands/server.js
+++ commands/server.js
@@ -1,27 +1,11 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { EmbedBuilder } = require('discord.js');
 
-module.exports = { 
-    data: new SlashCommandBuilder()
-        .setName('server')
-        .setDescription('Replies with server info!'),
-    run: async ({interaction}) => {
-        let embed = new EmbedBuilder()
-
-        embed
-        .setColor(0xE37800)
-        .setTitle(`${interaction.guild.name}'s Info`)
-        .setDescription(`Server info:`)
-        .setThumbnail(interaction.guild.iconURL())
-        .addFields({ name: 'Server Name', value: `${interaction.guild.name}`, inline: true },
-                    { name: 'Server Member Count', value: `${interaction.guild.memberCount}`, inline: true },
-                    { name: 'Server Boost Count', value: `${interaction.guild.premiumSubscriptionCount}`, inline: true },
-                    { name: 'Server Boost Level', value: `${interaction.guild.premiumTier}`, inline: true },
-                    { name: 'Server Created At', value: `${interaction.guild.createdAt}`, inline: true })
-        .setTimestamp(new Date())
-
-        await interaction.editReply({
-            embeds: [embed]
-        })
-    }
-}
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('server')
+		.setDescription('Provides information about the server.'),
+	async execute(interaction) {
+		// interaction.guild is the object representing the Guild in which the command was run
+		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+	},
+};
