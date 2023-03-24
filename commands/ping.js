5cbf1425b4f03ad7669ca85223ae4be95a447d92
Christoffer Riis <Christoffer@Riis.cc>
2023-03-24 23:42:18 +0100

    fixed ping command

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,8 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
-		await interaction.editReply({ content: 'This my edited second reply!' });
+		await interaction.reply({ content: 'Pong!', ephemeral: true });
+		const message = await interaction.fetchReply();
+		console.log(`Interaction reply fetched: ${message.content} (ID: ${message.id}) from ${message.author.tag} in ${message.channel.name} in ${message.guild.name}`);
 	},
 };
