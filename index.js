4275bfbb9dadd0045a9de03897a9f77a2b92ec12
Christoffer Riis <Christoffer@Riis.cc>
2023-03-25 13:47:20 +0100

    openai

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const { token, gptAPI, orgAPI } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMessages,
+	GatewayIntentBits.MessageContent,
+] });
 
 client.commands = new Collection();
 
@@ -14,6 +18,38 @@ const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'
 
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+
+// Setup OpenAI
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+	organization: orgAPI,
+	apiKey: gptAPI,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// uncomment this to test the openai
+/* client.on('messageCreate', async (message) => {
+	try {
+		// prevent infinite loops
+		if (message.author.bot) return;
+		const response = await openai.createCompletion({
+			model: 'text-davinci-003',
+			prompt: message.content,
+			temperature: 0.9,
+			max_tokens: 150,
+			top_p: 1,
+			frequency_penalty: 0.0,
+			presence_penalty: 0.6,
+			stop: [' Human:', ' AI:'],
+		});
+		message.reply(`${response.data.choices[0].text}`);
+	}
+	catch {
+		console.log('Error');
+	}
+}); */
 
 for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
